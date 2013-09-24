f14c56ef5be9d92a2ef89eee2480bf438feee3db
matthewepler <matthewepler@gmail.com>
2013-09-24 11:10:25 -0400

    initial commit

---
+++ models.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from mongoengine import *
+
+from flask.ext.mongoengine.wtf import model_form
+from datetime import datetime
+	
+class Candidate(Document):
+	# basic info
+	name = StringField(max_length=120, required=True)
+	slug = StringField()
+	tagline = StringField(required=True, verbose_name="What is your tagline?")
+	photo = StringField()
+	metrics = ListField(EmbeddedDocumentField(category))
+	timestamp = DateTimeField(default=datetime.now())
+
+# validation form
+CandidateForm = model_form(Candidate)
+
+
+class Category(EmbeddedDocument):
+	title = StringField(max_Length=120, required=True)
+	minValue = IntField(required=True)
+	maxValue = IntField(required=True)
+	currValue = DecimalField(force_string=True, precision=1)
+
+# validation form
+CategoryForm = model_form(Category)
+

8d7c8a9fd29d4e07e16cf556b1762d8fcf63566e
matthewepler <matthewepler@gmail.com>
2013-09-24 12:19:14 -0400

    cleanup

--- models.py
+++ models.py
@@ -3,22 +3,10 @@ from mongoengine import *
 
 from flask.ext.mongoengine.wtf import model_form
 from datetime import datetime
-	
-class Candidate(Document):
-	# basic info
-	name = StringField(max_length=120, required=True)
-	slug = StringField()
-	tagline = StringField(required=True, verbose_name="What is your tagline?")
-	photo = StringField()
-	metrics = ListField(EmbeddedDocumentField(category))
-	timestamp = DateTimeField(default=datetime.now())
-
-# validation form
-CandidateForm = model_form(Candidate)
 
 
 class Category(EmbeddedDocument):
-	title = StringField(max_Length=120, required=True)
+	title = StringField(max_length=120, required=True)
 	minValue = IntField(required=True)
 	maxValue = IntField(required=True)
 	currValue = DecimalField(force_string=True, precision=1)
@@ -26,3 +14,14 @@ class Category(EmbeddedDocument):
 # validation form
 CategoryForm = model_form(Category)
 
+class Candidate(Document):
+	# basic info
+	name = StringField(max_length=120, required=True)
+	slug = StringField()
+	tagline = StringField(required=True, verbose_name="What is your tagline?")
+	photo = StringField()
+	metrics = ListField(EmbeddedDocumentField(Category))
+	timestamp = DateTimeField(default=datetime.now())
+
+# validation form
+CandidateForm = model_form(Candidate)
